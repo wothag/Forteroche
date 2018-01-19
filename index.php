1a67e12463e44825419f2eeca8fa1503a7175036
wothag <d.r.c.hagnere@gmail.com>
2018-01-19 23:43:22 +0100

    ajout de de fonction commentaires

--- index.php
+++ index.php
@@ -7,10 +7,45 @@
  */
 
 
-require('model.php');
+require('controller/frontend.php');
 
-$chapters = getChapters();
+if (isset($_GET['action']))
+{
+    if ($_GET['action'] == 'listChapters')
+    {
+        listChapters();
+    }
+    elseif ($_GET['action'] == 'comments')
+    {
+        if (isset($_GET['id']) && $_GET['id'] > 0)
+        {
+            listComments();
+        } else
+            {
+            echo 'Attention : aucun identifiant de billet envoyé, il s\'agit d\'une erreur';
+            }
+    }
+	elseif ($_GET['action'] == 'addComment')
+	{
+		if (isset($_GET['id']) && $_GET['id'] > 0)
+		{
+			if (!empty($_POST['author']) && !empty($_POST['comment']))
+			{
+				addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+			}
+			else {
+				echo 'Erreur : tous les champs ne sont pas remplis !';
+			}
+		}
+		else
+		{
+			echo 'Erreur : aucun identifiant de billet envoyé';
+		}
+	}
+}
+ else {
+ listChapters();
+}
 
-require('indexView.php');
 
 
--- model/frontend.php
+++ model/frontend.php
@@ -44,6 +44,16 @@ function getComments($chapterId)
     return $comments;
 }
 
+function postComment($postId, $author, $comment)
+{
+	$db = dbConnect();
+	$comments = $db->prepare('INSERT INTO comments(post_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');
+	$affectedLines = $comments->execute(array($postId, $author, $comment));
+
+	return $affectedLines;
+}
+
+
 // Nouvelle fonction qui nous permet d'éviter de répéter du code
 function dbConnect()
 {
