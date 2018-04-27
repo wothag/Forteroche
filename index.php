047d1e3b836ec748ded69db77ce908ef9ca19e6a
littleBlackMan <littleblackman@users.noreply.github.com>
2018-04-27 19:34:41 +0200

    Update index.php
    supprimer les elements liés à la vue

--- index.php
+++ index.php
@@ -3,9 +3,6 @@ session_start();
 require('application/frontend/controller/frontend.php');
 require 'application/frontend/inc/header.php';
 
-
-
-
 try {
 	if (!isset($_GET['action'])) {
 		homePage();
@@ -17,15 +14,12 @@ try {
 			if (isset($_GET['id']) && $_GET['id'] > 0) {
 				post();
 			} else {
-
-				Echo ('Commentaire signalé merci');
+				echo ('Commentaire signalé merci');
 			}
-
 		} elseif ($_GET['action'] == 'addComment') {
 			if (isset($_GET['id']) && $_GET['id'] > 0) {
 				if (!empty($_POST['author']) && !empty($_POST['comment'])) {
 					addComment($_GET['id'], $_POST['author'], $_POST['comment']);
-
 				} else {
 					throw new Exception ('Erreur : tous les champs ne sont pas remplis !');
 				}
@@ -34,14 +28,16 @@ try {
 			}
 		} elseif ($_GET['action'] == 'flag' && ($_GET['id'] > 0)) {
 			flag($_GET['id']);
-
 		}
 	}
 }
 catch(Exception $e) {
 	echo 'Erreur : ' . $e->getMessage();
 }
-require 'application/frontend/inc/footer.php';
+// la vue ne doit pas être dans l'index.php 
+// l'index.php est le frontcontroller. Il contient le routeur
+// il doit faire lien entre la request-la route demandée et le controlleur a appelé (un objet ou une fonction)
+require 'application/frontend/inc/footer.php'; // a supprimer
 
 
 
