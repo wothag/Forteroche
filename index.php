20d4110caf7fb7b1b967489dbfea459d81387f00
wothag <33983572+wothag@users.noreply.github.com>
2018-04-30 01:54:35 +0200

    Refactoring + HTML and CSS corrections

--- index.php
+++ index.php
@@ -1,47 +1,65 @@
 <?php
 session_start();
-require('application/frontend/controller/frontend.php');
-require 'application/frontend/inc/header.php';
-
-
-
+require('application/frontend/Controller/FrontendController.php');
+require('application/frontend/Inc/header.php');
 
 try {
-	if (!isset($_GET['action'])) {
+	if (!isset($_GET['action']))
+	{
 		homePage();
 	}
-	else {
-		if ($_GET['action'] == 'listPosts') {
-			listPosts();
-		} elseif ($_GET['action'] == 'post') {
-			if (isset($_GET['id']) && $_GET['id'] > 0) {
-				post();
-			} else {
-
-				Echo ('Commentaire signalé merci');
+	else 
+	{
+		if ($_GET['action'] == 'listPosts')
+			{
+				if (isset($_GET['page']))
+					{
+						$page = $_GET['page'];
+					}
+			{	
+				listPosts();
 			}
-
-		} elseif ($_GET['action'] == 'addComment') {
-			if (isset($_GET['id']) && $_GET['id'] > 0) {
-				if (!empty($_POST['author']) && !empty($_POST['comment'])) {
-					addComment($_GET['id'], $_POST['author'], $_POST['comment']);
-
-				} else {
-					throw new Exception ('Erreur : tous les champs ne sont pas remplis !');
+		}	 
+		elseif ($_GET['action'] == 'post') 
+			{
+				if (isset($_GET['id']) && $_GET['id'] > 0) 
+				{
+					$postId = ($_GET['id']);
+					post($postId);
+				} 
+				else 
+				{
+					Echo ('Commentaire signalé merci');
 				}
-			} else {
-				throw new Exception ('Erreur : aucun identifiant de billet envoyé');
-			}
-		} elseif ($_GET['action'] == 'flag' && ($_GET['id'] > 0)) {
-			flag($_GET['id']);
-
+					} elseif ($_GET['action'] == 'addComment')
+					 	{
+						if (isset($_GET['id']) && $_GET['id'] > 0) 
+							{
+								if (!empty($_POST['author']) && !empty($_POST['comment']))
+									{
+										addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+									} 
+									else 
+										{
+											throw new Exception ('Erreur : tous les champs ne sont pas remplis !');
+										}
+							} 
+							else 
+								{
+									throw new Exception ('Erreur : aucun identifiant de billet envoyé');
+								}
+						} 
+						elseif ($_GET['action'] == 'flag' && ($_GET['id'] > 0)) 
+							{
+								flag($_GET['id']);
+							}
 		}
 	}
-}
-catch(Exception $e) {
+catch(Exception $e) 
+{
 	echo 'Erreur : ' . $e->getMessage();
 }
-require 'application/frontend/inc/footer.php';
+
 
 
 
